c5af191b80d8ba61ab67e25a222448b7513c2046
timcartwright <tim@websitesbytim.com>
2015-12-06 00:57:48 +0530

    Styling

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,9 +1,19 @@
 class OrderItemsController < ApplicationController
 
+  respond_to :html, :js
+
   def create
     item = MenuItem.find(params[:item])
     restaurant = Restaurant.find(params[:restaurant])
     current_user.add_to_order(item, restaurant)
     redirect_to restaurant_path(restaurant)
+  end
+
+  def destroy
+    
+    item = OrderItem.find(params[:id])
+    item.destroy
+
+    redirect_to request.referrer
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   resources :order_items
   resources :orders
   resources :charges
