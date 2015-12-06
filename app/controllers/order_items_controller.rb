46a2dcb0c4c4c696370626e7300f82bcaefd840b
timcartwright <tim@websitesbytim.com>
2015-12-06 19:48:25 +0530

    Handle first item to empty order with AJAX

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,8 +6,10 @@ class OrderItemsController < ApplicationController
     item = MenuItem.find(params[:item])
     restaurant = Restaurant.find(params[:restaurant])
     @order_item = current_user.add_to_order(item, restaurant)
-    @total_price = @order_item.order.total_price
+    @order = @order_item.order
+    @total_price = @order.total_price
     @quantity = @order_item.quantity
+    @orderitems = @order.orderitems
   end
 
   def destroy
@@ -16,10 +18,14 @@ class OrderItemsController < ApplicationController
     amount = @order_item.price
     if @order_item.quantity == 1
       @order_item.destroy
+      @quantity = 0
     else
       @order_item.quantity -= 1
+      @quantity = @order_item.quantity
       @order_item.save
     end
+
+
     
     @order_item.order.total_price -= amount
     @total_price = @order_item.order.total_price
